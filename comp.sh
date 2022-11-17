64cf0f6ebbc9b5f69c0a6eb3c2faef1f0e66fe67
Damon Gonzalez <dbg3220@rit.edu>
2022-11-17 12:15:28 -0500

    Added bash script to automate compilation

---
+++ comp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Modify this compilation file whenever a new C source file is added
+
+CFLAGS="-std=c99 -ggdb -Wall -Wextra"
+C_FILES="main.c database.c student.c"
+OBJ_FILES="database.o student.o ListADT.o"
+TARGETS=("main")
+
+gcc $CFLAGS -c $C_FILES
+echo "Compiled $C_FILES into object files"
+
+for target in ${TARGETS[@]}; do
+	gcc $CFLAGS -o $target "${target}.o" $OBJ_FILES
+	echo "Linked ${target}.o $OBJ_FILES to create executable '$target'"
+done
+
+rm *.o
+echo "Removed object files to clean directory"
+
