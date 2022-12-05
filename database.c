971f2c4b7e8df9a36a667bae44f35d8262683867
Damon Gonzalez <dbg3220@rit.edu>
2022-12-04 19:18:16 -0500

    Reformatted code

--- database.c
+++ database.c
@@ -22,43 +22,43 @@ typedef struct Database_H {
 /// @param database - the database to be loaded
 /// @return 1 if the data file was successfully read, 0 if there was a conflict
 static int load( Database database ){
-	FILE * file = fopen( database->file, "r" );
-	if( !file ){
-		return 0;
-	}
-	char line[200];
-	while ( fgets( line, 200, file ) != NULL ){
-		if( line[ strlen( line ) - 1 ] == '\n' ){
-			line[ strlen( line ) - 1 ] = '\0';//trims the final character if it is a newline character
-		}
-		char* firstName = strtok( line, "," );
-		char* lastName = strtok( NULL, "," );
-		char* email = strtok( NULL, "," );
-		int age = (int) strtol( strtok( NULL, "," ), NULL, 10 );
-		double gpa = strtod( strtok( NULL, "," ), NULL );
-		Student student = student_create( firstName, lastName, email, age, gpa );
-		list_append( database->students, (void *) student );
-	}
-	fclose( file );
-	return 1;
+    FILE * file = fopen( database->file, "r" );
+    if( !file ){
+        return 0;
+    }
+    char line[200];
+    while ( fgets( line, 200, file ) != NULL ){
+        if( line[ strlen( line ) - 1 ] == '\n' ){
+            line[ strlen( line ) - 1 ] = '\0';//trims the final character if it is a newline character
+        }
+        char* firstName = strtok( line, "," );
+        char* lastName = strtok( NULL, "," );
+        char* email = strtok( NULL, "," );
+        int age = (int) strtol( strtok( NULL, "," ), NULL, 10 );
+        double gpa = strtod( strtok( NULL, "," ), NULL );
+        Student student = student_create( firstName, lastName, email, age, gpa );
+        list_append( database->students, (void *) student );
+    }
+    fclose( file );
+    return 1;
 }
 
 /// Saves the database to the file originally specified
 ///
 /// @param database - the database to be saved
 static void save( Database database ){
-	FILE * output = fopen( database->file, "w" );
-	int length = list_size( database->students );
-	for( int i = 0; i < length; i++ ){
-		Student s = (Student) list_get( database->students, i );
-		char* firstName = student_getFirstName( s );
-		char* lastName = student_getLastName( s );
-		char* email = student_getEmail( s );
-		int age = student_getAge( s );
-		double gpa = student_getGPA( s );
-		fprintf( output, "%s,%s,%s,%d,%.2f\n", firstName, lastName, email, age, gpa );
-	}
-	fclose( output );
+    FILE * output = fopen( database->file, "w" );
+    int length = list_size( database->students );
+    for( int i = 0; i < length; i++ ){
+        Student s = (Student) list_get( database->students, i );
+        char* firstName = student_getFirstName( s );
+        char* lastName = student_getLastName( s );
+        char* email = student_getEmail( s );
+        int age = student_getAge( s );
+        double gpa = student_getGPA( s );
+        fprintf( output, "%s,%s,%s,%d,%.2f\n", firstName, lastName, email, age, gpa );
+    }
+    fclose( output );
 }
 
 /// Returns the index of a student in the database with the given
@@ -68,149 +68,149 @@ static void save( Database database ){
 /// @param email The email to match against
 /// @return The index of the student, -1 if no student found
 static int indexOf( Database database, char* email ){
-	int length = list_size( database->students );
-	for( int i = 0; i < length; i++ ){
-		Student s = (Student) list_get( database->students, i );
-		if( strcmp( email, student_getEmail( s ) ) == 0 ){
-			return i;
-		}
-	}
-	return -1;
+    int length = list_size( database->students );
+    for( int i = 0; i < length; i++ ){
+        Student s = (Student) list_get( database->students, i );
+        if( strcmp( email, student_getEmail( s ) ) == 0 ){
+            return i;
+        }
+    }
+    return -1;
 }
 
 Database database_create( char* file ){
     Database database = (Database)malloc( sizeof( struct Database_H ) );
     database->file = file;
     database->students = list_create( student_equals, student_toString );
-	int result = load( database );
-	if( !result ){
-		list_destroy( database->students );
-		free( database );
-		return NULL;
-	}
+    int result = load( database );
+    if( !result ){
+        list_destroy( database->students );
+        free( database );
+        return NULL;
+    }
     return database;
 }
 
 void database_exit( Database database ){
-	save( database );
-	while( list_size( database->students ) != 0 ){
-		Student student = (Student) list_get( database->students, 0 );
-		list_delete( database->students, 0 );
-		student_destroy( student );
-	}
-	list_destroy( database->students );
-	free( database );
+    save( database );
+    while( list_size( database->students ) != 0 ){
+        Student student = (Student) list_get( database->students, 0 );
+        list_delete( database->students, 0 );
+        student_destroy( student );
+    }
+    list_destroy( database->students );
+    free( database );
 }
 
 void database_force_exit( Database database ){
-	while( list_size( database->students ) != 0 ){
-		Student student = (Student) list_get( database->students, 0 );
-		list_delete( database->students, 0 );
-		student_destroy( student );
-	}
-	list_destroy( database->students );
-	free( database );
+    while( list_size( database->students ) != 0 ){
+        Student student = (Student) list_get( database->students, 0 );
+        list_delete( database->students, 0 );
+        student_destroy( student );
+    }
+    list_destroy( database->students );
+    free( database );
 }
 
 ListADT database_get( Database database ){
-	ListADT list = list_create( student_equals, student_toString );
-	int length = list_size( database->students );
-	for( int i = 0; i < length; i++ ){
-		list_append( list, list_get( database->students, i ) );
-	}
-	return list;
+    ListADT list = list_create( student_equals, student_toString );
+    int length = list_size( database->students );
+    for( int i = 0; i < length; i++ ){
+        list_append( list, list_get( database->students, i ) );
+    }
+    return list;
 }
 
 Student database_getByEmail( Database database, char* email ){
-	int length = list_size( database->students );
-	for( int i = 0; i < length; i++ ){
-		Student s = (Student) list_get( database->students, i );
-		if( strcmp( student_getEmail( s ), email ) == 0 ){
-			return s;
-		}
-	}
-	return NULL;
+    int length = list_size( database->students );
+    for( int i = 0; i < length; i++ ){
+        Student s = (Student) list_get( database->students, i );
+        if( strcmp( student_getEmail( s ), email ) == 0 ){
+            return s;
+        }
+    }
+    return NULL;
 }
 
 ListADT database_getByFirstName( Database database, char* firstName ){
-	ListADT list = list_create( student_equals, student_toString );
-	int length = list_size( database->students );
-	for( int i = 0; i < length; i++ ){
-		Student s = (Student) list_get( database->students, i );
-		if( strcmp( student_getFirstName( s ), firstName ) == 0 ){
-			list_append( list, (void *) s );
-		}
-	}
-	return list;
+    ListADT list = list_create( student_equals, student_toString );
+    int length = list_size( database->students );
+    for( int i = 0; i < length; i++ ){
+        Student s = (Student) list_get( database->students, i );
+        if( strcmp( student_getFirstName( s ), firstName ) == 0 ){
+            list_append( list, (void *) s );
+        }
+    }
+    return list;
 }
 
 ListADT database_getByLastName( Database database, char* lastName ){
-	ListADT list = list_create( student_equals, student_toString );
-	int length = list_size( database->students );
-	for( int i = 0; i < length; i++ ){
-		Student s = (Student) list_get( database->students, i );
-		if( strcmp( student_getLastName( s ), lastName ) == 0 ){
-			list_append( list, (void *) s );
-		}
-	}
-	return list;
+    ListADT list = list_create( student_equals, student_toString );
+    int length = list_size( database->students );
+    for( int i = 0; i < length; i++ ){
+        Student s = (Student) list_get( database->students, i );
+        if( strcmp( student_getLastName( s ), lastName ) == 0 ){
+            list_append( list, (void *) s );
+        }
+    }
+    return list;
 }
 
 ListADT database_getByAge( Database database, int age ){
-	ListADT list = list_create( student_equals, student_toString );
-	int length = list_size( database->students );
-	for( int i = 0; i < length; i++ ){
-		Student s = (Student) list_get( database->students, i );
-		if( student_getAge( s ) == age ){
-			list_append( list, (void *) s );
-		}
-	}
-	return list;
+    ListADT list = list_create( student_equals, student_toString );
+    int length = list_size( database->students );
+    for( int i = 0; i < length; i++ ){
+        Student s = (Student) list_get( database->students, i );
+        if( student_getAge( s ) == age ){
+            list_append( list, (void *) s );
+        }
+    }
+    return list;
 }
 
 ListADT database_getByGPA( Database database, double low, double high ){
-	ListADT list = list_create( student_equals, student_toString );
-	int length = list_size( database->students );
-	for( int i = 0; i < length; i++ ){
-		Student s = (Student) list_get( database->students, i );
-		double gpa = student_getGPA( s );
-		if( !(gpa < low) && !(gpa > high) ){
-			list_append( list, (void *) s );
-		}
-	}
-	return list;
+    ListADT list = list_create( student_equals, student_toString );
+    int length = list_size( database->students );
+    for( int i = 0; i < length; i++ ){
+        Student s = (Student) list_get( database->students, i );
+        double gpa = student_getGPA( s );
+        if( !(gpa < low) && !(gpa > high) ){
+            list_append( list, (void *) s );
+        }
+    }
+    return list;
 }
 
 bool database_add( Database database, Student student ){
-	char* email = student_getEmail( student );
-	if( indexOf( database, email ) != -1 ){
-		return false;
-	}
-	list_append( database->students, student );	
-	return true;
+    char* email = student_getEmail( student );
+    if( indexOf( database, email ) != -1 ){
+        return false;
+    }
+    list_append( database->students, student );	
+    return true;
 }
 
 bool database_update( Database database, Student student ){
-	char* email = student_getEmail( student );
-	int index = indexOf( database, email );
-	if( index == -1 ){
-		return false;
-	}
-	Student s = (Student) list_get( database->students, index );
-	free( s );
-	list_delete( database->students, index );
-	list_add( database->students, (void *)student, index );
-	return true;
+    char* email = student_getEmail( student );
+    int index = indexOf( database, email );
+    if( index == -1 ){
+        return false;
+    }
+    Student s = (Student) list_get( database->students, index );
+    free( s );
+    list_delete( database->students, index );
+    list_add( database->students, (void *)student, index );
+    return true;
 }
 
 bool database_delete( Database database, char* email ){
-	int index = indexOf( database, email );
-	if( index == -1 ){
-		return false;
-	}
-	Student s = (Student) list_get( database->students, index );
-	free( s );
-	list_delete( database->students, index );
-	return true;
+    int index = indexOf( database, email );
+    if( index == -1 ){
+	    return false;
+    }
+    Student s = (Student) list_get( database->students, index );
+    free( s );
+    list_delete( database->students, index );
+    return true;
 }
 
--- main.c
+++ main.c
@@ -7,19 +7,19 @@
 
 #include "database.h"
 
-#define USAGE       		"Usage: ./main <database_file>"
+#define USAGE               "Usage: ./main <database_file>"
 #define ERROR_NOT_FOUND     "Command Not Found, type 'help' to see a list of relevant commands"
-#define ERROR_USE_GET		"You must use get to bring up a list of students to modify"
-#define COMMANDS    		"'quit': Ends the program and saves all changes\n"\
-							"'fquit': Ends the program without saving changes\n"\
-                    		"'get': Retrieves students from the database. You can get all or search by name, email, age, or gpa\n"\
-							"'next': Displays the next 10 students\n"\
-							"'clear': Clears the current students from usage\n"\
-							"'add': Adds a new student to the database\n"\
-							"'update': Updates a selected student\n"\
-							"'delete': Deletes a selected student\n"\
-                    		"'help: Shows a list of helpful commands"
-#define PROMPT				"-> "
+#define ERROR_USE_GET       "You must use get to bring up a list of students to modify"
+#define COMMANDS            "'quit': Ends the program and saves all changes\n"\
+                            "'fquit': Ends the program without saving changes\n"\
+                            "'get': Retrieves students from the database. You can get all or search by name, email, age, or gpa\n"\
+                            "'next': Displays the next 10 students\n"\
+                            "'clear': Clears the current students from usage\n"\
+                            "'add': Adds a new student to the database\n"\
+                            "'update': Updates a selected student\n"\
+                            "'delete': Deletes a selected student\n"\
+                            "'help: Shows a list of helpful commands"
+#define PROMPT              "-> "
 
 /// Displays 10 students in the list starting with the student at index num. If
 /// an invalid index is given(i.e. < 0 or > list_size) then the first 10
@@ -31,21 +31,21 @@
 /// @param list - the list to be analyzed
 /// @param num - the index of the first student to be displayed 
 static void display( ListADT list, int num ){
-	int size = list_size( list );
-	int last_index;
-	if( num < 0 || !(num < size ) ){
-		num = 0;
-	}
-	for( int i = num; i < num + 10 && i < size; i++ ){
-		Student s = (Student) list_get( list, i );
-		char* str = student_toString( s );
-		printf( "%d. %s\n", i, str );
-		free( str );
-		last_index = i;
-	}
-	if( last_index < size - 1 ){
-		printf( "...(%d total students)\n", size );
-	}
+    int size = list_size( list );
+    int last_index;
+    if( num < 0 || !(num < size ) ){
+        num = 0;
+    }
+    for( int i = num; i < num + 10 && i < size; i++ ){
+        Student s = (Student) list_get( list, i );
+        char* str = student_toString( s );
+        printf( "%d. %s\n", i, str );
+        free( str );
+        last_index = i;
+    }
+    if( last_index < size - 1 ){
+        printf( "...(%d total students)\n", size );
+    }
 }
 
 /// @brief main function of this program
@@ -57,82 +57,82 @@ int main( int argv, char* argc[] ){
         return EXIT_FAILURE;
     } 
 
-	printf( "Loading data from: %s\n", argc[1] );
+    printf( "Loading data from: %s\n", argc[1] );
     Database database = database_create( argc[1] );
-	if( database == NULL ){
-		printf( "Error reading from <%s>, could not open database\n", argc[1] );
-		return EXIT_FAILURE;
+    if( database == NULL ){
+        printf( "Error reading from <%s>, could not open database\n", argc[1] );
+        return EXIT_FAILURE;
 	}
 
     char input[50];
-	ListADT list = NULL;
-	int num = 0;
+    ListADT list = NULL;
+    int num = 0;
     while( true ){
         printf( PROMPT );
         scanf( "%49s", input );
 
         if( strcmp( input, "quit" ) == 0 ){//quit command ends the program
-		    printf( "Saving database to file\n" );
-    		database_exit( database );
+            printf( "Saving database to file\n" );
+            database_exit( database );
             break;
         } else if( strcmp( input, "fquit" ) == 0 ){
-			printf( "Closing database without saving changes\n" );
-			database_force_exit( database );
-			break;
-		} else if( strcmp( input, "get" ) == 0 ){//get command gives user students to view
-			list = database_get( database );
-			display( list, 0 );
+            printf( "Closing database without saving changes\n" );
+            database_force_exit( database );
+            break;
+        } else if( strcmp( input, "get" ) == 0 ){//get command gives user students to view
+            list = database_get( database );
+            display( list, 0 );
         } else if( strcmp( input, "next" ) == 0 ){//next command shows more students
-			if( list != NULL ){
-			} else {
-				printf( "%s\n", ERROR_USE_GET );
-			}
-		} else if( strcmp( input, "clear" ) == 0 ){//clear command removes the current students from view
-			list_destroy( list );
-			list = NULL;
-		} else if( strcmp( input, "add" ) == 0 ){//add command lets user add a student
-		} else if( strcmp( input, "update" ) == 0 ){//update command lets user update a student
-			if( list != NULL ){
-				int index;
-				int listSize = list_size( list );
-				while( true ){
-					printf( "Enter student #-> " );
-					int result = scanf( "%d", &index );
-					if( result == 0 ){
-						printf( "Error reading in your input, please try again\n" );
-					} else if( index < 1 || index > listSize ){
-				 		printf( "Invalid student #, please try again\n" );
-					} else {
-						break;
-					}
-				}
-				Student student = (Student) list_get( list, index );
-				//TODO update that student
-			} else {
-				printf( "%s\n", ERROR_USE_GET );
-			}
-		} else if( strcmp( input, "delete" ) == 0 ){//delete command lets user delete a student
-			if( list != NULL ){
-				int index;
-				int listSize = list_size( list );
-				while( true ){
-					printf( "Enter student #-> " );
-					int result = scanf( "%d", &index );
-					if( EOF == result ){
-						printf( "Error reading in your input, please try again\n" );
-					} else if( index < 1 || index > listSize ){
-				 		printf( "Invalid student #, please try again\n" );
-					} else {
-						break;
-					}
-				}
-				Student student = (Student) list_get( list, index );
-				char* email = student_getEmail( student );
-				database_delete( database, email );
-			} else {
-				printf( "%s\n", ERROR_USE_GET );
-			}
-		} else if ( strcmp( input, "help" ) == 0 ){//help command shows user useful commands
+            if( list != NULL ){
+            } else {
+                printf( "%s\n", ERROR_USE_GET );
+            }
+        } else if( strcmp( input, "clear" ) == 0 ){//clear command removes the current students from view
+            list_destroy( list );
+            list = NULL;
+        } else if( strcmp( input, "add" ) == 0 ){//add command lets user add a student
+        } else if( strcmp( input, "update" ) == 0 ){//update command lets user update a student
+            if( list != NULL ){
+                int index;
+                int listSize = list_size( list );
+                while( true ){
+                    printf( "Enter student #-> " );
+                    int result = scanf( "%d", &index );
+                    if( result == 0 ){
+                        printf( "Error reading in your input, please try again\n" );
+                    } else if( index < 1 || index > listSize ){
+                        printf( "Invalid student #, please try again\n" );
+                    } else {
+                        break;
+                    }
+                }
+                Student student = (Student) list_get( list, index );
+                //TODO update that student
+            } else {
+                printf( "%s\n", ERROR_USE_GET );
+            }
+        } else if( strcmp( input, "delete" ) == 0 ){//delete command lets user delete a student
+            if( list != NULL ){
+                int index;
+                int listSize = list_size( list );
+                while( true ){
+                    printf( "Enter student #-> " );
+                    int result = scanf( "%d", &index );
+                    if( EOF == result ){
+                        printf( "Error reading in your input, please try again\n" );
+                    } else if( index < 1 || index > listSize ){
+                        printf( "Invalid student #, please try again\n" );
+                    } else {
+                        break;
+                    }
+                }
+                Student student = (Student) list_get( list, index );
+                char* email = student_getEmail( student );
+                database_delete( database, email );
+            } else {
+                printf( "%s\n", ERROR_USE_GET );
+            }
+        } else if ( strcmp( input, "help" ) == 0 ){//help command shows user useful commands
             printf( "%s\n", COMMANDS );
         } else {//default, if no command recognized shows user an error
             printf( "%s\n", ERROR_NOT_FOUND );
