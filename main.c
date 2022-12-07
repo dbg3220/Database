f77aafed400010b680edc76702a544165797684c
Damon Gonzalez <dbg3220@rit.edu>
2022-12-07 12:57:41 -0500

    Deleted majority of user interface for refactoring

--- main.c
+++ main.c
@@ -97,136 +97,14 @@ int main( int argv, char* argc[] ){
             printf( "Closing database without saving changes\n" );
             database_force_exit( database );
             break;
-        } else if( strcmp( command, "get" ) == 0 ){//get command gives user students to view
-            if( list != NULL ){
-                list_destroy( list );
-                list = NULL;
-            }
-            num = 0;
-            command = strtok( NULL, " " );
-            if( command == NULL ){
-                printf( "get requires at least 1 additional argument\n" );
-            } else if( strcmp( command, "all" ) == 0 ){
-                list = database_get( database );
-                display( list, num );
-                printf( "...(%d total students found)\n", list_size( list ) );
-            } else if( strcmp( command, "age" ) == 0 ) {
-                command = strtok( NULL, " " );
-                if( command == NULL ){
-                    printf( "get age requires an age parameter, i.e. 'get age 19'\n" );
-                } else {
-                    int age = strtol( command, NULL, 10 );
-                    list = database_getByAge( database, age );
-                    display( list, num );
-                    printf( "...(%d total students found)\n", list_size( list ) );
-                }
-            } else if( strcmp( command, "gpa" ) == 0 ){
-                double lower, upper;
-                char* args[2];
-                args[0] = strtok( NULL, " " );
-                args[1] = strtok( NULL, " " );
-                if( args[0] == NULL || args[1] == NULL ){
-                    printf( "get gpa requires two parameters, a lower bound and upper bound\n" );
-                } else {
-                    lower = strtod( args[0], NULL );
-                    upper = strtod( args[1], NULL );
-                    list = database_getByGPA( database, lower, upper );
-                    display( list, num );
-                    printf( "...(%d total students found)\n", list_size( list ) );
-                }
-            } else if( strcmp( command, "firstname" ) == 0 ){
-                command = strtok( NULL, " " );
-                if( command == NULL ){
-                    printf( "get firstname requires a parameters, i.e. 'get firstname Damon'\n" );
-                } else {
-                    list = database_getByFirstName( database, command );
-                    display( list, num );
-                    printf( "...(%d total students found)\n", list_size( list ) );
-                }
-            } else if( strcmp( command, "lastname" ) == 0 ){
-                command = strtok( NULL, " " );
-                if( command == NULL ){
-                    printf( "get lastname requires a parameter, i.e. 'get lastname Gonzalez'\n" );
-                } else {
-                    list = database_getByLastName( database, command );
-                    display( list, num );
-                    printf( "...(%d total students found)\n", list_size( list ) );
-                }
-            } else{
-                printf( "'%s' not recognized, you can use all, lastname, age, gpa\n", command );
-            }
-        } else if( strcmp( command, "next" ) == 0 ){//next command shows more students
-            if( list != NULL ){
-                num += 10;
-                if( num >= list_size( list ) ){
-                    num = 0;
-                }
-                display( list, num );
-                if( num + 9 < list_size( list ) - 1 ){
-                    printf( "...(%d total students)\n", list_size( list ) - 1 );
-                }
-            } else {
-                printf( "%s\n", ERROR_USE_GET );
-            }
-        } else if( strcmp( command, "clear" ) == 0 ){//clear command removes the current students from view
-            list_destroy( list );
-            list = NULL;
-            num = 0;
-        } else if( strcmp( command, "add" ) == 0 ){//add command lets user add a student
-            char firstname[buffer_size];
-            char lastname[buffer_size];
-            char email[buffer_size];
-            int age;
-            double gpa;
-            printf( "%s firstname-> ", PROMPT );
-            input( buffer_size, firstname );
-            printf( "%s lastname-> ", PROMPT );
-            input( buffer_size, lastname );
-            printf( "%s email-> ", PROMPT );
-            input( buffer_size, email );
-            printf( "%s age-> ", PROMPT );
-            input( buffer_size, buffer );
-            age = (int) strtol( buffer, NULL, 10 );
-            printf( "%s gpa-> ", PROMPT );
-            input( buffer_size, buffer );
-            gpa = strtod( buffer, NULL );
-            Student student = student_create( firstname, lastname, email, age, gpa );
-            char* str = student_toString( student );
-            if( database_add( database, student ) ){
-                printf( "%s was successfully added\n", str );
-            } else {
-                printf( "This student's email already exists in the database, enter a unique email.\n" );
-                free( student );
-            }   
-        } else if( strcmp( command, "update" ) == 0 ){//update command lets user update a student
-            if( list != NULL ){
-            } else {
-                printf( "%s\n", ERROR_USE_GET );
-            }
-        } else if( strcmp( command, "delete" ) == 0 ){//delete command lets user delete a student
-            if( list != NULL ){
-                command = strtok( NULL, " " );
-                if( command == NULL ){
-                    printf( "delete requires 1 more parameter, i.e. delete 0\n" );
-                } else {
-                    int student_num = (int) strtol( command, NULL, 10 );
-                    if( student_num >= 0 && student_num < list_size( list ) ){
-                        Student s = (Student) list_get( list, student_num );
-                        char* email = student_getEmail( s );
-                        database_delete( database, email );
-                    } else {
-                        printf( "%d is not a valid index\n", student_num );
-                    }
-                }
-
-            } else {
-                printf( "%s\n", ERROR_USE_GET );
-            }
         } else if ( strcmp( command, "help" ) == 0 ){//help command shows user useful commands
             printf( "%s\n", COMMANDS );
         } else {//default, if no command recognized shows user an error
             printf( "%s\n", ERROR_NOT_FOUND );
         }
+    }
+    if( list != NULL ){
+        list_destroy( list );
     }
 
     return EXIT_SUCCESS;
